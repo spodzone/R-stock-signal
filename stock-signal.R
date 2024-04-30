d3f7e99e46c0d631c96ee4992218385a99645897
Tim Haynes <github@stirfried.vegetable.org.uk>
2024-04-30 13:11:16 +0100

    Add block to calculate stats   * bollinger bands percentage   * RSI   * gradients   * all over 15,30,90d windows   * also z-score versions of the same as appropriate

--- stock-signal.R
+++ stock-signal.R
@@ -13,6 +13,11 @@ stock_symbols <- c("FSLR","AMD","CSIQ","MSFT","AAPL",
 start_date <- as.Date("2020-01-01")
 end_date <- Sys.Date()  # Today's date
 
+gradient <- function (data) {
+  model <- lm( y~x, data=data.frame(x=1:length(data), y=data))
+  return (as.numeric(model$coefficients[2]))
+}
+
 data <- hash()
 
 # Loop through each stock symbol and download data
@@ -28,5 +33,34 @@ for(symbol in stock_symbols) {
     write.csv(stock_data, file = paste0(symbol, "_data.csv"))
   }, error = function(e) {
     cat("Error downloading data for", symbol, ":", conditionMessage(e), "\n\n")
+  })
+}
+
+cat("Calculating stats\n")
+stats <- hash()
+normstats <- hash()
+for(symbol in stock_symbols) {
+  tryCatch({
+    stats[[symbol]] <- data.frame(bb15=BBands(data[[symbol]]$Close, n=15)[,'pctB'])
+    normstats[[symbol]] <- data.frame(bb15=scale(stats[[symbol]]$bb15))
+    stats[[symbol]]$bb30 <- BBands(data[[symbol]]$Close, n=30)[,'pctB']
+    normstats[[symbol]]$bb30 <- scale(stats[[symbol]]$bb30)
+    stats[[symbol]]$bb90 <- BBands(data[[symbol]]$Close, n=90)[,'pctB']
+    normstats[[symbol]]$bb90 <- scale(stats[[symbol]]$bb90)
+    stats[[symbol]]$rsi15 <- RSI(data[[symbol]]$Close, n=15)
+    normstats[[symbol]]$rsi15 <- scale(stats[[symbol]]$rsi15)
+    stats[[symbol]]$rsi30 <- RSI(data[[symbol]]$Close, n=30)
+    normstats[[symbol]]$rsi30 <- scale(stats[[symbol]]$rsi30)
+    stats[[symbol]]$rsi90 <- RSI(data[[symbol]]$Close, n=90)
+    normstats[[symbol]]$rsi90 <- scale(stats[[symbol]]$rsi90)
+    # calculate gradient
+    stats[[symbol]]$gradient15 <- gradient(tail(data[[symbol]]$Close, 15))
+    normstats[[symbol]]$gradient15 <- stats[[symbol]]$gradient15
+    stats[[symbol]]$gradient30 <- gradient(tail(data[[symbol]]$Close, 30))
+    normstats[[symbol]]$gradient30 <- stats[[symbol]]$gradient30
+    stats[[symbol]]$gradient90 <- gradient(tail(data[[symbol]]$Close, 90))
+    normstats[[symbol]]$gradient90 <- stats[[symbol]]$gradient90
+  }, error=function(e) {
+    cat("Error calculating stats for ", symbol,":", conditionMessage(e), "\n\n")
   })
 }
