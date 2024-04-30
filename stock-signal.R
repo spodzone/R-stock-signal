cf4eb540580af4b8efcdf7cb13b1f3ebe8d0e700
Tim Haynes <github@stirfried.vegetable.org.uk>
2024-04-30 13:09:00 +0100

    Add block to download all the watched symbols

--- stock-signal.R
+++ stock-signal.R
@@ -14,3 +14,19 @@ start_date <- as.Date("2020-01-01")
 end_date <- Sys.Date()  # Today's date
 
 data <- hash()
+
+# Loop through each stock symbol and download data
+cat("Downloading\n")
+for(symbol in stock_symbols) {
+  tryCatch({
+    # Download the stock data
+    stock_data <- getSymbols(symbol, src = "yahoo", from = start_date, to = end_date, auto.assign = FALSE)
+
+    data[[symbol]] <- data.frame(tstmp=attr(stock_data, "index"), price=stock_data[,4])
+    colnames(data[[symbol]]) <- c("tstmp", "Close")
+    # Write the data to a CSV file
+    write.csv(stock_data, file = paste0(symbol, "_data.csv"))
+  }, error = function(e) {
+    cat("Error downloading data for", symbol, ":", conditionMessage(e), "\n\n")
+  })
+}
